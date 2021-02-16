f784cb41e0c6c813065eb36394a46884f28982d2
Sergey Novikov <sergey.novikov@flaconi.de>
2021-02-16 13:40:12 +0100

    Initial set of resources

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "s3_origin_hostname" {
+  description = "Hostname of S3-bucket to be used as origin"
+  type        = string
+}
+
+variable "s3_logging_hostname" {
+  description = "Hostname of S3-bucket to be used for logging"
+  type        = string
+  default     = ""
+}
+
+variable "r53_hostname" {
+  description = "Hostname for CloudFront alias"
+  type        = string
+}
+
+variable "r53_zone_id" {
+  description = "Route53 zone ID to be used for hostname and certificate validation"
+  type        = string
+}
+
+variable "tags" {
+  description = "Map of custom tags for the provisioned resources"
+  type        = map
+  default     = {}
+}
