5202f56af1602117871594f372e01b37ad86cc15
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-17 09:13:24 +0200

    Update module versions

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
-output "this_certificate_arn" {
+output "certificate_arn" {
   description = "ARN of ACM SSL certificate created for CloudFront"
-  value       = module.certificate.this_acm_certificate_arn
+  value       = module.certificate.acm_certificate_arn
 }
 
-output "this_cloudfront_arn" {
+output "cloudfront_arn" {
   description = "ARN of CloudFront distribution creates"
-  value       = module.cloudfront.this_cloudfront_distribution_arn
+  value       = module.cloudfront.cloudfront_distribution_arn
 }
 
-output "this_cloudfront_id" {
+output "cloudfront_id" {
   description = "ID CloudFront distribution ID"
-  value       = module.cloudfront.this_cloudfront_distribution_id
+  value       = module.cloudfront.cloudfront_distribution_id
 }
 
-output "this_cloudfront_hosted_zone_id" {
+output "cloudfront_hosted_zone_id" {
   description = "Hosted Zone ID CloudFront distribution uses"
-  value       = module.cloudfront.this_cloudfront_distribution_hosted_zone_id
+  value       = module.cloudfront.cloudfront_distribution_hosted_zone_id
 }
 
-output "this_cloudfront_alias" {
+output "cloudfront_alias" {
   description = "Alias hostname of CloudFront distribution"
   value       = aws_route53_record.this.fqdn
 }
